433918b2fe82d7a4725486deb249724c07d507ae
pabon98 <hasinanjumpabon@gmail.com>
2022-11-30 09:33:18 +0600

    updated -url

--- src/Components/MyOrders/MyOrders.js
+++ src/Components/MyOrders/MyOrders.js
@@ -11,7 +11,7 @@ const MyOrders = () => {
     const { user } = useAuth();
 
     useEffect(() => {
-        const url = `https://tour-planner-server-site.vercel.app/myOrders/${user?.email}`;
+        const url = `https://fast-brushlands-79885.herokuapp.com/myOrders/${user?.email}`;
        
         fetch(url)
             .then(res => res.json())
@@ -19,7 +19,7 @@ const MyOrders = () => {
     }, [user.email])
 
     useEffect(() => {
-        const url = `https://tour-planner-server-site.vercel.app/services`;
+        const url = `https://fast-brushlands-79885.herokuapp.com/services`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -50,7 +50,7 @@ const MyOrders = () => {
         // console.log(id);
         const proceed = window.confirm('Are you sure, you want to cancel the order?')
         if (proceed) {
-            const url = `https://tour-planner-server-site.vercel.app/${id}`;
+            const url = `https://fast-brushlands-79885.herokuapp.com/${id}`;
             
             fetch(url, {
                 method: 'DELETE'
--- src/Components/PlaceOrder/PlaceOrder.js
+++ src/Components/PlaceOrder/PlaceOrder.js
@@ -16,7 +16,7 @@ const PlaceOrder = () => {
         data.order = services._id;
         data.status = 'pending';
 
-        const url = `https://tour-planner-server-site.vercel.app/placeOrder`;
+        const url = `https://fast-brushlands-79885.herokuapp.com/placeOrder`;
        
         fetch(url, {
             method: 'POST',
@@ -35,7 +35,7 @@ const PlaceOrder = () => {
     };
 
     useEffect(() => {
-        const url = `https://tour-planner-server-site.vercel.app/services/${serviceId}`;
+        const url = `https://fast-brushlands-79885.herokuapp.com/services/${serviceId}`;
         
         fetch(url)
             .then(res => res.json())
